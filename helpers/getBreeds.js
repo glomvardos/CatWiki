b9b785ba76345c8bc11ffdb0d99cd39d875d5b1c
glomvardos <gerapes90@yahoo.gr>
2021-06-16 22:08:05 +0300

    search bar changes and env variables added

--- helpers/getBreeds.js
+++ helpers/getBreeds.js
@@ -1,7 +1,7 @@
 export async function getBreeds() {
   const response = await fetch('https://api.thecatapi.com/v1/breeds', {
     headers: {
-      'x-api-key': '98c49160-f776-4bb1-8c9e-1e37287c0537',
+      'x-api-key': process.env.API_KEY,
     },
   })
 
@@ -15,7 +15,7 @@ export async function getBreedImages(id) {
     `https://api.thecatapi.com/v1/images/search?limit=8&breed_id=${id}`,
     {
       headers: {
-        'x-api-key': '98c49160-f776-4bb1-8c9e-1e37287c0537',
+        'x-api-key': process.env.API_KEY,
       },
     }
   )
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     MONGODB_USERNAME: process.env.MONGODB_USERNAME,
     MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
     MONGODB_NAME: process.env.MONGODB_NAME,
+    API_KEY: process.env.API_KEY,
   },
   images: {
     domains: ['cdn2.thecatapi.com'],
