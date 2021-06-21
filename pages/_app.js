83307ec36df00d8196eb3a6ce384d2f6b87f915a
glomvardos <gerapes90@yahoo.gr>
2021-06-21 19:38:04 +0300

    meta tag duplication fixed

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
-        <meta charset='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link
           rel='stylesheet'
